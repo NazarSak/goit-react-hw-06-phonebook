4ac8d0bda498f9d5bec8fa0a2781c246553484ff
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-04-17 11:09:20 +0300

    123

--- src/redux/ContactSlice.js
+++ src/redux/ContactSlice.js
@@ -1,8 +1,12 @@
-
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  contacts: [],
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
 };
 
 const contactsSlice = createSlice({
